f57304b73c48048b3570da1c4371305ff5f2be9b
Junior Paixao <juniortidesenvolvimento1@gmail.com>
2024-08-06 16:46:04 -0300

    segundo commit

--- ECommerce/GeekShopping/GeekShopping.ProductAPI/Model/Product.cs
+++ ECommerce/GeekShopping/GeekShopping.ProductAPI/Model/Product.cs
@@ -1,6 +1,22 @@
-﻿namespace GeekShopping.ProductAPI.Model
+﻿using GeekShopping.ProductAPI.Model.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductAPI.Model
 {
-    public class Product
+    [Table("product")]
+    public class Product : BaseEntity
     {
+        [Column("nome")]
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Column("price")]
+        [Required]
+        [Range(1,10000)]
+        public decimal Price { get; set; }
+
+
     }
 }
